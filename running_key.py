627aa070c8cd35b91871c5b11a8256ef3b31b4d4
melisa829 <melisa.grajcevci@student.uni-pr.edu>
2026-04-18 00:14:32 +0200

    Update running_key.py

--- running_key.py
+++ running_key.py
@@ -1,1 +1,33 @@
+def running_key_encrypt(text, key):
+    text = text.replace(" ", "").upper()
+    key = key.replace(" ", "").upper()
 
+    if len(key) < len(text):
+        return "Key duhet me qenë më i gjatë se text!"
+
+    result = ""
+
+    for i in range(len(text)):
+        p = ord(text[i]) - ord('A')
+        k = ord(key[i]) - ord('A')
+
+        c = (p + k) % 26
+        result += chr(c + ord('A'))
+
+    return result
+
+
+def running_key_decrypt(cipher, key):
+    cipher = cipher.upper()
+    key = key.upper()
+
+    result = ""
+
+    for i in range(len(cipher)):
+        c = ord(cipher[i]) - ord('A')
+        k = ord(key[i]) - ord('A')
+
+        p = (c - k + 26) % 26
+        result += chr(p + ord('A'))
+
+    return result
