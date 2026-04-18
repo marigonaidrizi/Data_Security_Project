9313e98cba8664250eaba161043e312f8851a9f7
marigonaidrizi <129410282+marigonaidrizi@users.noreply.github.com>
2026-04-18 16:29:51 +0200

    Implementova funksionin single_transposition_decrypt
    I cili nevojitet për dekriptimin e tekstit në algoritmin Double Transposition

--- double_transposition.py
+++ double_transposition.py
@@ -1,1 +1,34 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+def single_transposition_decrypt(cipher, key):
+    cols = len(key)
+    rows = math.ceil(len(cipher) / cols)
+
+    key_order = sorted(list(enumerate(key)), key=lambda x: x[1])
+
+    matrix = [['' for _ in range(cols)] for _ in range(rows)]
+
+    index = 0
+    for col_index, _ in key_order:
+        for row in range(rows):
+            if index < len(cipher):
+                matrix[row][col_index] = cipher[index]
+                index += 1
+
+    result = ""
+    for row in matrix:
+        result += "".join(row)
+
+    return result
